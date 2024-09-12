1a8635f8bf06339f93d910568539658d39c07771
Tanjim Hasan <tanjimhassan666.gmail.com>
2024-09-12 22:34:43 +0600

    change

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Array of book objects
 const books = [
     { id: 1, title: "The Midnight Library", author: "Matt Haig", category: "Fiction", price: 14.99, image: "images/book1.jpg" },
     { id: 2, title: "Educated", author: "Tara Westover", category: "Non-fiction", price: 19.99, image: "images/book2.jpg" },
@@ -5,7 +6,10 @@ const books = [
     { id: 4, title: "The Silent Patient", author: "Alex Michaelides", category: "Mystery", price: 11.99, image: "images/book4.jpg" }
 ];
 
+// Cart array
 const cart = [];
+
+// Get DOM elements
 const bookList = document.getElementById('book-list');
 const cartItems = document.getElementById('cartItems');
 const cartTotal = document.getElementById('cartTotal');
@@ -25,9 +29,9 @@ function displayFeaturedBooks() {
 }
 
 // Function to display books
-function displayBooks(books) {
+function displayBooks(booksToDisplay) {
     bookList.innerHTML = '';
-    books.forEach(book => {
+    booksToDisplay.forEach(book => {
         const bookItem = document.createElement('div');
         bookItem.classList.add('book-item');
         bookItem.innerHTML = `
@@ -77,6 +81,6 @@ document.getElementById('searchBar').addEventListener('input', function(e) {
     displayBooks(filteredBooks);
 });
 
-// Initial load
-displayFeaturedBooks();
+// Initialize page
 displayBooks(books);
+displayFeaturedBooks();
